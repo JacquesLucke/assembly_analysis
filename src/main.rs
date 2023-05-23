f942bbe4044db0499a2b72eb5a18730e84843e3e
Jacques Lucke <mail@jlucke.com>
2023-05-23 11:33:23 +0200

    cleanup error handling

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use eyre::Result;
 use serde::Deserialize;
 use std::{collections::HashMap, io::Read, process::Command};
 
@@ -26,11 +27,16 @@ impl<'a> FunctionInfo<'a> {
     }
 }
 
-fn main() {
+fn load_cmake_compile_commands(path: &std::path::Path) -> Result<Vec<CMakeCompileCommand>> {
+    let file = std::fs::File::open(path)?;
+    let compile_commands: Vec<CMakeCompileCommand> = serde_json::from_reader(file)?;
+    Ok(compile_commands)
+}
+
+fn app() -> Result<()> {
     let compile_commands_path = "/home/jacques/blender/build_debug/compile_commands.json";
-    let file = std::fs::File::open(compile_commands_path).expect("Couldn't load compile commands");
-    let compile_commands: Vec<CMakeCompileCommand> =
-        serde_json::from_reader(file).expect("Couldn't parse file");
+    let compile_commands =
+        load_cmake_compile_commands(std::path::Path::new(compile_commands_path))?;
 
     let mut command_by_output = std::collections::hash_map::HashMap::new();
     for command in &compile_commands {
@@ -39,39 +45,33 @@ fn main() {
 
     let command = command_by_output
         .get("source/blender/modifiers/CMakeFiles/bf_modifiers.dir/intern/MOD_uvwarp.cc.o")
-        .expect("Didn't find command");
+        .ok_or(eyre::eyre!("Can't find compile command."))?;
 
-    let mut args = shlex::split(&command.command).expect("Failed to split command");
-    let output_index = args.iter().position(|x| x == "-o").expect("Can't find -o");
+    let mut args =
+        shlex::split(&command.command).ok_or(eyre::eyre!("Can't split cmake command."))?;
+    let output_index = args
+        .iter()
+        .position(|x| x == "-o")
+        .ok_or(eyre::eyre!("Can't find -o in the command."))?;
     let mut assembly_file_path =
         std::path::Path::new(&command.directory).join(&args[output_index + 1]);
     assembly_file_path.set_extension("txt");
-    args[output_index + 1] = assembly_file_path.to_str().unwrap().to_owned();
+    args[output_index + 1] = assembly_file_path
+        .to_str()
+        .ok_or(eyre::eyre!("Failed to create assembly output path."))?
+        .to_owned();
     args.insert(output_index, "-S".to_owned());
 
-    let command = Command::new(&args[0])
+    let mut command = Command::new(&args[0])
         .args(&args[1..])
         .current_dir(&command.directory)
-        .spawn();
-    match command {
-        Ok(mut child) => {
-            let exit_status = child.wait().expect("Failed");
-            if exit_status.success() {
-                println!("Process existed successfully");
-            } else {
-                println!("Error");
-            }
-        }
-        Err(err) => {
-            println!("Failed: {}", err);
-        }
+        .spawn()?;
+    if !command.wait()?.success() {
+        return Err(eyre::eyre!("Generating assembly failed."));
     }
 
     let mut assembly = String::new();
-    std::fs::File::open(&assembly_file_path)
-        .unwrap()
-        .read_to_string(&mut assembly)
-        .unwrap();
+    std::fs::File::open(&assembly_file_path)?.read_to_string(&mut assembly)?;
 
     let mut info_by_symbol: HashMap<&str, FunctionInfo> =
         std::collections::hash_map::HashMap::new();
@@ -89,7 +89,10 @@ fn main() {
                 let info = info_by_symbol.get_mut(symbol).unwrap();
 
                 if line.starts_with("call") {
-                    let mangled_name = line.split_ascii_whitespace().nth(1).unwrap();
+                    let mangled_name = line
+                        .split_ascii_whitespace()
+                        .nth(1)
+                        .ok_or(eyre::eyre!("Couldn't parse function name."))?;
                     info.callees.insert(mangled_name);
                 }
                 if !line.starts_with(".") {
@@ -102,6 +105,16 @@ fn main() {
 
     info_by_symbol.retain(|_, x| x.instructions_num > 0);
 
-    println!("{:#?}", info_by_symbol)
+    println!("{:#?}", info_by_symbol);
     // std::fs::remove_file(assembly_file_path).expect("Can't remove file");
+    Ok(())
+}
+
+fn main() {
+    match app() {
+        Ok(_) => {}
+        Err(err) => {
+            println!("{:?}", err);
+        }
+    }
 }
